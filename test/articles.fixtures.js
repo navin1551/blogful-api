80c099d0f57d0b2738aa0b4aff295315b41e1151
Navin Kumar <navin1551@gmail.com>
2019-05-16 22:14:26 -0700

    refactoring to work with relational databases

--- test/articles.fixtures.js
+++ test/articles.fixtures.js
@@ -6,7 +6,8 @@ function makeArticlesArray() {
       title: "First test post!",
       style: "How-to",
       content:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus consequuntur deserunt commodi, nobis qui inventore corrupti iusto aliquid debitis unde non.Adipisci, pariatur.Molestiae, libero esse hic adipisci autem neque ?"
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus consequuntur deserunt commodi, nobis qui inventore corrupti iusto aliquid debitis unde non.Adipisci, pariatur.Molestiae, libero esse hic adipisci autem neque ?",
+      author: 1
     },
     {
       id: 2,
@@ -14,7 +15,8 @@ function makeArticlesArray() {
       title: "Second test post!",
       style: "News",
       content:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, exercitationem cupiditate dignissimos est perspiciatis, nobis commodi alias saepe atque facilis labore sequi deleniti. Sint, adipisci facere! Velit temporibus debitis rerum."
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, exercitationem cupiditate dignissimos est perspiciatis, nobis commodi alias saepe atque facilis labore sequi deleniti. Sint, adipisci facere! Velit temporibus debitis rerum.",
+      author: 2
     },
     {
       id: 3,
@@ -22,7 +24,8 @@ function makeArticlesArray() {
       title: "Third test post!",
       style: "Listicle",
       content:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus, voluptate? Necessitatibus, reiciendis? Cupiditate totam laborum esse animi ratione ipsa dignissimos laboriosam eos similique cumque. Est nostrum esse porro id quaerat."
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus, voluptate? Necessitatibus, reiciendis? Cupiditate totam laborum esse animi ratione ipsa dignissimos laboriosam eos similique cumque. Est nostrum esse porro id quaerat.",
+      author: 3
     },
     {
       id: 4,
@@ -30,11 +33,33 @@ function makeArticlesArray() {
       title: "Fourth test post!",
       style: "Story",
       content:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum molestiae accusamus veniam consectetur tempora, corporis obcaecati ad nisi asperiores tenetur, autem magnam. Iste, architecto obcaecati tenetur quidem voluptatum ipsa quam?"
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum molestiae accusamus veniam consectetur tempora, corporis obcaecati ad nisi asperiores tenetur, autem magnam. Iste, architecto obcaecati tenetur quidem voluptatum ipsa quam?",
+      author: 4
     }
   ];
 }
 
+function makeMaliciousArticle() {
+  const maliciousArticle = {
+    id: 911,
+    style: "How-to",
+    date_published: new Date().toISOString(),
+    title: 'Naughty naughty very naughty <script>alert("xss");</script>',
+    content: `Bad image <img src="https://url.to.file.which/does-not.exist" onerror="alert(document.cookie);">. But not <strong>all</strong> bad.`
+  };
+  const expectedArticle = {
+    ...maliciousArticle,
+    title:
+      'Naughty naughty very naughty &lt;script&gt;alert("xss");&lt;/script&gt;',
+    content: `Bad image <img src="https://url.to.file.which/does-not.exist">. But not <strong>all</strong> bad.`
+  };
+  return {
+    maliciousArticle,
+    expectedArticle
+  };
+}
+
 module.exports = {
-  makeArticlesArray
+  makeArticlesArray,
+  makeMaliciousArticle
 };
